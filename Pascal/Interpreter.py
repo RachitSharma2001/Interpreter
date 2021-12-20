9e1b9ec9e4c8331de2df50d66d238392c52bc26c
RachitSharma2001 <rachitsharma613@gmail.com>
2021-12-20 11:39:39 -0800

    Part 4 basically done - converted grammer to code

--- Pascal/Interpreter.py
+++ Pascal/Interpreter.py
@@ -4,70 +4,62 @@ class Interpreter(object):
     def __init__(self, command):
         self.command = command
         self.pos = 0
-    
-    def run(self):
-        tokens_list = self.get_tokens()
-        return self.parse(tokens_list)
 
-    def get_tokens(self):
-        tokens_list = []
-        while self.is_next_token():
-            tokens_list.append(self.get_next_token())
-        print(tokens_list)
-        return tokens_list
+    def run(self):
+        if not self.is_next_token:
+            raise Exception("Syntax error - requirement of at least one token")
+        self.get_next_token()
+        return self.parse()
     
-    '''
-        This assumes that the current token is not a space
-    '''
     def get_next_token(self):
-        curr_token = self.command[self.pos]
-        if curr_token.isdigit():
+        token = self.command[self.pos]
+        if token.isdigit():
             self.pos += 1
             while self.is_next_token():
-                next_token = self.peek()
+                next_token = self.command[self.pos]
                 if not next_token.isdigit():
                     break
-                curr_token += next_token
+                token += next_token
                 self.pos += 1
-            return Token(curr_token)
-        elif curr_token == '+' or curr_token == '-':
+            self.curr_token = Token(token)
+        elif token == '+' or token == '-':
             self.pos += 1
-            return Token(curr_token)
+            self.curr_token = Token(token)
         else:
-            raise Exception("Invalid token: " + curr_token)
-
-    def peek(self):
-        if not self.is_next_token():
-            raise Exception("Error in code: called peek but no next token")
-        return self.command[self.pos]
+            raise Exception("Invalid token: " + token)
     
-    '''
-        Returns true if there is a non space token after this current position
-        Return false otherwise
-    '''
     def is_next_token(self):
         # Ignore all the current spaces
         while self.pos < len(self.command) and self.command[self.pos] == ' ':
             self.pos += 1
         return self.pos < len(self.command)
     
-    def parse(self, tokens_list):
-        if len(tokens_list) == 1 and tokens_list[0].is_type(INTEGER):
-            return tokens_list[0].get_value()
-        elif len(tokens_list) >= 3 and len(tokens_list) % 2 == 1 and tokens_list[0].is_type(INTEGER):
-            sum = tokens_list[0].get_value()
+    def eat(self, type):
+        print("Curr token: ", self.curr_token)
+        if type == INTEGER and self.curr_token.is_type(type):
+            res = self.curr_token.get_value()
+            if self.is_next_token():
+                self.get_next_token()
+            return res
+        elif (type == PLUS or type == MINUS) and self.curr_token.is_type(type):
+            if self.is_next_token():
+                self.get_next_token()
+            return 0
+        raise Exception("Syntax error - unable to understand token")
+    
+    def factor(self):
+        return self.eat(INTEGER)
 
-            for ind in range(1, len(tokens_list), 2):
-                if tokens_list[ind+1].is_type(INTEGER):
-                    if tokens_list[ind].is_type(PLUS):
-                        sum += tokens_list[ind+1].get_value()
-                    elif tokens_list[ind].is_type(MINUS):
-                        sum -= tokens_list[ind+1].get_value()
-                    else:
-                        raise Exception("Unidentified Expression given")
-                else:
-                    raise Exception("Unidentified Expression given")
-
-            return sum
-        else:
-            raise Exception("Unidentified Expression given")
+    def parse(self):
+        print(self.curr_token)
+        sum = self.factor()
+        while self.curr_token.is_type(PLUS) or self.curr_token.is_type(MINUS):
+            if self.curr_token.is_type(PLUS):
+                self.eat(PLUS)
+                sum += self.factor()
+            else:
+                self.eat(MINUS)
+                sum -= self.factor()
+        if self.is_next_token():
+            raise Exception("Syntax error - unable to understand token")
+        return sum
