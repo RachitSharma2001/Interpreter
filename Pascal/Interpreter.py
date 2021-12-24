622de3306880839349320a2a50324ad94a89758c
RachitSharma2001 <rachitsharma613@gmail.com>
2021-12-24 09:49:38 -0800

    Lexer done

--- Pascal/Interpreter.py
+++ Pascal/Interpreter.py
@@ -1,7 +1,68 @@
-from Token import Token
-from Token import INTEGER, PLUS, MINUS, MUL, DIV, LPAREN, RPAREN
-from Token import BEGIN, END, DOT, ID, ASSIGN, SEMI
+BEGIN, END, DOT, ID, ASSIGN, SEMI = 'Begin', 'End', 'Dot', 'Id', 'Assign', 'Semi'
+PROGRAM, COMMA, COLON, REAL, INTEGER_DIV, FLOAT_DIV = 'Program', 'Comma', 'Colon', 'Real', 'IntDiv', 'FloatDiv'
+INTEGER_CONST, REAL_CONST, INTEGER, PLUS, MINUS, MUL, LPAREN, RPAREN = 'IntConst', 'RealConst', 'Integer', 'Plus', 'Minus', 'MUL', '(', ')'
 from Ast import Regular, Compound, Assign, Variable
+
+class Token(object):
+    def interpret_type(self, lexeme):
+        if lexeme == 'BEGIN':
+            return BEGIN
+        elif lexeme == 'END':
+            return END
+        elif lexeme == 'PROGRAM':
+            return PROGRAM 
+        elif lexeme == 'REAL':
+            return REAL
+        elif lexeme == 'INTEGER':
+            return INTEGER
+        elif lexeme == ',':
+            return COMMA
+        elif lexeme == ':':
+            return COLON 
+        elif lexeme == 'DIV':
+            return INTEGER_DIV
+        elif lexeme == '.':
+            return DOT
+        elif lexeme == ':=':
+            return ASSIGN 
+        elif lexeme == ';':
+            return SEMI
+        elif lexeme[0].isdigit() and '.' in lexeme:
+            return REAL_CONST
+        elif lexeme.isdigit():
+            return INTEGER_CONST
+        elif lexeme == '+':
+            return PLUS
+        elif lexeme == '-':
+            return MINUS
+        elif lexeme == '*':
+            return MUL
+        elif lexeme == '/':
+            return FLOAT_DIV
+        elif lexeme == '(':
+            return LPAREN
+        elif lexeme == ')':
+            return RPAREN
+        # if its none of the above, then likely a variable
+        return ID
+    
+    def __init__(self, lexeme):
+        self.curr_token = (self.interpret_type(lexeme), lexeme)
+    
+    def is_type(self, given_type):
+        return self.curr_token[0] == given_type
+
+    def get_value(self):
+        if self.curr_token[1].isdigit():
+            return int(self.curr_token[1])
+        return self.curr_token[1]
+    
+    def get_type(self):
+        return self.curr_token[0]
+
+    def __repr__(self):
+        return '({}, {})'.format(self.curr_token[0], self.curr_token[1])
+
 class Interpreter(object):
     def __init__(self, command):
         self.command = command
@@ -9,14 +70,14 @@ class Interpreter(object):
 
     def run(self):
         self.curr_token = self.get_next_token()
-        '''while self.curr_token != None:
+        while self.curr_token != None:
             print(self.curr_token)
-            self.curr_token = self.get_next_token()'''
-        if self.curr_token == None:
+            self.curr_token = self.get_next_token()
+        '''if self.curr_token == None:
             raise Exception("Syntax error - requirement of at least one token")
         parsed_ast = self.program()
         print(parsed_ast.post_order())
-        return self.interpret(parsed_ast)
+        return self.interpret(parsed_ast)'''
     
     # Functions for the lexer  
     ''' Returns the next token ''' 
@@ -25,27 +86,25 @@ class Interpreter(object):
         if self.at_end():
             return None 
         token = self.command[self.pos]
-        if token in ('+', '-', '*', '/', '(', ')', ';', '.'):
+        if self.pos < len(self.command) - 1 and self.command[self.pos:self.pos+2] == ':=':
+            self.pos += 2
+            return Token(':=')
+        elif token in ('+', '-', '*', '/', '(', ')', ';', '.', ':', ','):
             self.pos += 1
             return Token(token)
         elif token.isdigit():
             self.pos += 1
             while not self.at_end():
                 next_token = self.command[self.pos]
-                if not next_token.isdigit():
+                if not next_token.isdigit() and next_token != '.':
                     break
                 token += next_token
                 self.pos += 1
             return Token(token)
-        elif self.pos < len(self.command) - 1 and self.command[self.pos:self.pos+2] == ':=':
-            self.pos += 2
-            return Token(':=')
         elif token.isalnum():
             self.pos += 1
             while not self.at_end():
                 curr_char = self.command[self.pos]
-                if (token == 'Begin' and curr_char == ' '):
-                    break 
                 if not curr_char.isalnum():
                     break
                 token += curr_char
