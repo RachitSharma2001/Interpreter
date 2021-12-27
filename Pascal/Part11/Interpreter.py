caa4b0dc8f7ad7f6a1df71cca5b17a87e35d114a
RachitSharma2001 <rachitsharma613@gmail.com>
2021-12-27 11:12:04 -0800

    Part 11 directory

---
+++ Pascal/Part11/Interpreter.py
@@ -0,0 +1,74 @@
+from Token import *
+from Lexer import Lexer
+from Parser import Parser
+from SymbolInterpreter import SymbolInterpreter
+import sys
+
+class Interpreter(object):
+    def __init__(self, command):
+        self.lexer = Lexer(command)
+        self.global_vars = {}
+    
+    def run(self):
+        parser = Parser(self.lexer)
+        ast_node = parser.generate_ast()
+        # Checks if any errors with symbols
+        sym_int = SymbolInterpreter()
+        sym_int.interpret(ast_node)
+        self.interpret(ast_node)
+        return self.global_vars
+
+    def interpret(self, ast_node):
+        self.visit_post_order(ast_node)
+    
+    def visit_post_order(self, ast_node):
+        name_of_method = 'visit_post_order_' + type(ast_node).__name__
+        visitor = getattr(self, name_of_method, self.error_visit)
+        return visitor(ast_node)
+
+    def error_visit(self, ast_node):
+        raise Exception('No {} method exists'.format(type(ast_node).__name__))
+
+    def visit_post_order_Constant(self, ast_node):
+        return ast_node.get_value()
+
+    def visit_post_order_UnOp(self, ast_node):
+        value = self.visit_post_order(ast_node.get_child())
+        return (-1 if ast_node.is_negative() else 1) * value
+    
+    def visit_post_order_BinOp(self, ast_node):
+        left_side = self.visit_post_order(ast_node.get_left_child())
+        right_side = self.visit_post_order(ast_node.get_right_child())
+        op = ast_node.get_operand()
+        if op == '+':
+            return left_side + right_side 
+        elif op == '-':
+            return left_side - right_side
+        elif op == '*':
+            return left_side * right_side 
+        elif op == '/':
+            return left_side / right_side
+        elif op == 'DIV':
+            return int(left_side / right_side)
+    
+    def visit_post_order_Variable(self, ast_node, get_value=True):
+        name = ast_node.get_value()
+        if get_value:
+            return self.global_vars[name]
+        return name
+    
+    def visit_post_order_Assign(self, ast_node):
+        value = self.visit_post_order(ast_node.get_value())
+        var_name = ast_node.get_variable().get_value()
+        self.global_vars[var_name] = value 
+    
+    def visit_post_order_Var_decl(self, ast_node):
+        self.global_vars[ast_node.get_name()] = -sys.maxsize
+    
+    def visit_post_order_Compound(self, ast_node):
+        for child in ast_node.get_children():
+            self.visit_post_order(child)
+    
+    def visit_post_order_Block(self, ast_node):
+        for child in ast_node.get_children():
+            self.visit_post_order(child)
---
+++ Pascal/Part11/Token.py
@@ -0,0 +1,87 @@
+PROCEDURE = 'PROCEDURE'
+BEGIN = 'Begin'
+END = 'End'
+DOT = 'Dot'
+ID = 'Id'
+ASSIGN = 'Assign'
+SEMI = 'Semi'
+VAR = 'VAR'
+PROGRAM = 'Program'
+COMMA = 'Comma'
+COLON = 'Colon'
+REAL = 'Real'
+INTEGER_DIV = 'IntDiv'
+FLOAT_DIV = 'FloatDiv'
+INTEGER_CONST = 'IntConst'
+REAL_CONST = 'RealConst'
+INTEGER = 'Integer'
+PLUS = 'Plus'
+MINUS = 'Minus'
+MUL = 'Mul'
+LPAREN = '('
+RPAREN = ')'
+
+
+class Token(object):
+    def interpret_type(self, lexeme):
+        if lexeme == 'PROCEDURE':
+            return PROCEDURE
+        if lexeme == 'BEGIN':
+            return BEGIN
+        elif lexeme == 'END':
+            return END
+        elif lexeme == 'PROGRAM':
+            return PROGRAM 
+        elif lexeme == 'REAL':
+            return REAL
+        elif lexeme == 'INTEGER':
+            return INTEGER
+        elif lexeme == 'VAR':
+            return VAR
+        elif lexeme == ',':
+            return COMMA
+        elif lexeme == ':':
+            return COLON 
+        elif lexeme == 'DIV':
+            return INTEGER_DIV
+        elif lexeme == '.':
+            return DOT
+        elif lexeme == ':=':
+            return ASSIGN 
+        elif lexeme == ';':
+            return SEMI
+        elif lexeme[0].isdigit() and '.' in lexeme:
+            return REAL_CONST
+        elif lexeme.isdigit():
+            return INTEGER_CONST
+        elif lexeme == '+':
+            return PLUS
+        elif lexeme == '-':
+            return MINUS
+        elif lexeme == '*':
+            return MUL
+        elif lexeme == '/':
+            return FLOAT_DIV
+        elif lexeme == '(':
+            return LPAREN
+        elif lexeme == ')':
+            return RPAREN
+        # if its none of the above, then likely a variable
+        return ID
+    
+    def __init__(self, lexeme):
+        self.curr_token = (self.interpret_type(lexeme), lexeme)
+    
+    def is_type(self, given_type):
+        return self.curr_token[0] == given_type
+
+    def get_value(self):
+        if self.curr_token[1].isdigit():
+            return int(self.curr_token[1])
+        return self.curr_token[1]
+    
+    def get_type(self):
+        return self.curr_token[0]
+
+    def __repr__(self):
+        return '({}, {})'.format(self.curr_token[0], self.curr_token[1])
