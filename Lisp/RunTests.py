3eb07bcbfb635832a11364d1dd36c08e3cb563ac
RachitSharma2001 <rachitsharma613@gmail.com>
2021-12-30 15:23:30 -0800

    Added Lisp interpreter

---
+++ Lisp/RunTests.py
@@ -0,0 +1,5 @@
+# Get each file from Tests/Input
+    # Enter the files data into Interpreter, and get the output
+    # Get the corresponding file in Tests/Output
+    # Compare the interpreter output to the real output
+    # If not correct, print out "Test # [num] failed, with output [interpreter output]"
